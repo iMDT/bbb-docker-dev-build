7cbe48996375e9f1e0e04c92f34eb2bc0e54070e
Tiago Jacobs <tiago@imdt.com.br>
2021-11-26 09:58:23 -0300

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -8,8 +8,8 @@ docker login -u $DOCKER_USER -p $DOCKER_PASS
 echo "Docker commit"
 docker commit bbb_docker_build $IMAGE_NAME
 echo "Docker tag"
-docker tag $IMAGE_NAME imdt/bigbluebutton:develop
-docker tag $IMAGE_NAME imdt/bigbluebutton:develop_build_$CIRCLE_BUILD_NUM
+docker tag $IMAGE_NAME imdt/bigbluebutton:2.5.x-develop
+docker tag $IMAGE_NAME imdt/bigbluebutton:2.5.x-develop_build_$CIRCLE_BUILD_NUM
 echo "Docker push"
 docker push imdt/bigbluebutton
 echo "Docker logout"
