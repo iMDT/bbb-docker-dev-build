3471d64b4600e93b6571159d9a99881dfe40637e
Gustavo Trott <gustavo@trott.com.br>
2025-01-31 10:09:25 -0300

    avoid nginx cache for client locales

--- docker/assets/setup.sh
+++ docker/assets/setup.sh
@@ -122,7 +122,7 @@ sudo git clone -b gh-pages --single-branch https://github.com/mconf/api-mate.git
 apt install -y pkg-config dh-autoreconf ncurses-dev build-essential libssl-dev libpcap-dev libncurses5-dev libsctp-dev lksctp-tools cmake
 git clone --recurse-submodules https://github.com/SIPp/sipp.git /opt/sipp
 cd /opt/sipp
-git checkout 4682fdba2b63007f13a632c6eb06f0ece84cb7df #Set an old commit once the current code is not working
+git checkout 4682fdba2b63007f13a632c6eb06f0ece84cb7df #Set an old commit as the current code is not working
 cmake . -DUSE_SSL=1 -DUSE_SCTP=1 -DUSE_PCAP=1 -DUSE_GSL=1
 make
 sudo make install
