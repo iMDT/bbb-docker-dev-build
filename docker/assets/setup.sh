eeb7cb099353178b8f4eec32c404cf6b6ac37204
Anton Georgiev <antobinary@users.noreply.github.com>
2025-11-21 10:32:51 -0500

    Update grails, gradle to match BBB 3.0.17

--- docker/assets/setup.sh
+++ docker/assets/setup.sh
@@ -174,8 +174,8 @@ su bigbluebutton -c bash -l << 'EOF'
     curl -s "https://get.sdkman.io" | bash
     source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-    sdk install gradle 7.3.1
-    sdk install grails 5.3.2
+    sdk install gradle 8.14.3
+    sdk install grails 7.0.0
     sdk install sbt 1.6.2
     sdk install maven 3.5.0
 
